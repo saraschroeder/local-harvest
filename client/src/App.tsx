7ed00d80fec91afeb097c8b1551519ffb0d92ac9
mcquo011 <omcquown@gmail.com>
2023-05-16 18:13:57 -0500

    style changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,67 +5,59 @@ import "../assets/css/header.css";
 import logo from "../assets/images/logo.png";
 
 function Header() {
-  // Assume this isLoggedIn state would actually come from your auth logic
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const isLoggedIn = useAuth()
 
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <div className="row w-100">
-          <div className="col-lg-4">
-            <Link className="navbar-brand" to="/">
-              <img src={logo} alt="Logo" className="logo" />
-            </Link>
-          </div>
-          <div className="col-lg-4 text-center d-none d-lg-block">
-            <h1 className="navbar-title">Local Harvest</h1>
-          </div>
-          <div className="col-lg-4">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse justify-content-end"
-              id="navbarNav"
-            >
-              <ul className="navbar-nav">
-                <li className="nav-item active">
-                  <Link className="nav-link" to="/home">
-                    Locate
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" className="logo" />
+          <span className="navbar-title ml-3">Local Harvest</span>
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/home">
+                Shop
+              </Link>
+            </li>
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart">
+                    Cart
                   </Link>
                 </li>
-                {isLoggedIn ? (
-                  <>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/cart">
-                        Cart
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/logout">
-                        | Logout
-                      </Link>
-                    </li>
-                  </>
-                ) : (
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                      Login
-                    </Link>
-                  </li>
-                )}
-              </ul>
-            </div>
-          </div>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/logout">
+                    Logout
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+            )}
+          </ul>
         </div>
       </div>
     </nav>
