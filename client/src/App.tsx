f31ba3b5c036f89f3138243a98c757d7d4a5d0cc
mcquo011 <omcquown@gmail.com>
2023-05-16 11:20:23 -0500

    login and sign up page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 import "./App.css";
-
 
 function App() {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </Router>
   );
 }
