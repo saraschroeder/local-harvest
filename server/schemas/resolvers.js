e28fdbc3782452cf4810e04bad2b742477acb56e
JSlander <jhovanslander@hotmail.com>
2023-05-16 19:46:05 -0500

    user.js, some resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,39 @@
 //resolvers
+const User = require("./user");
+
+const userResolvers = {
+  Query: {
+    users: () => User.find(),
+    userById: (_, { userId }) => User.findById(userId),
+  },
+  Mutation: {
+    createUser: (_, { email, userName, password, role, businessName, location }) => {
+      const user = new User({
+        email,
+        userName,
+        password,
+        role,
+        businessName,
+        location
+      });
+      return user.save();
+    },
+    updateUser: (_, { userId, email, userName, password, role, businessName, location }) => {
+      return User.findByIdAndUpdate(
+        userId,
+        { email, userName, password, role, businessName, location },
+        { new: true }
+      );
+    },
+    deleteUser: (_, { userId }) => {
+      return User.findByIdAndDelete(userId);
+    },
+  },
+  User: {
+    posts: (parent) => {
+      return Post.find({ _id: { $in: parent.posts } });
+    },
+  },
+};
+
+module.exports = userResolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,28 @@
 //typeDefs
+const { gql } = require("apollo-server");
+
+const userTypeDefs = gql`
+  type User {
+    _id: ID!
+    email: String!
+    userName: String!
+    password: String!
+    role: String!
+    businessName: String
+    location: String
+    posts: [Post]
+  }
+
+  type Query {
+    users: [User]
+    userById(userId: ID!): User
+  }
+
+  type Mutation {
+    createUser(email: String!, userName: String!, password: String!, role: String!, businessName: String): User
+    updateUser(userId: ID!, email: String, userName: String, password: String, role: String, businessName: String): User
+    deleteUser(userId: ID!): User
+  }
+`;
+
+module.exports = userTypeDefs;
