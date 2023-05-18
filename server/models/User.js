dc6fde16ad559056d071db1893dad3f1d0ae2253
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-17 22:57:10 -0500

    Adding Posts as ref; Added bcrypt code

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// Import schema from Post.js
-const postSchema = require('./Post')
-
 const userSchema = new Schema({
   email: {
     type: String,
@@ -33,8 +30,27 @@ const userSchema = new Schema({
     type: String,
   },
   image: { data: Buffer, contentType: String }, // Image field
-  post: [postSchema],
+  post: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
 });
+
+// Below code is from Module 21 Challenge
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.verifyPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 
