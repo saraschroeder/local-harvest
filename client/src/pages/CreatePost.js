b22844cbf4247f831199d341de5562be9941b551
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-20 00:35:26 -0500

    Added page to create post using mutation

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useMutation } from "@apollo/client";
+import { CREATE_POST } from '../utils/mutations';
+
+function CreatePost() {
+    // Set initial form state
+    const [formState, setFormState] = useState({
+        title: "",
+        image: "",
+        description: "",
+        price: ""
+      });
+
+    // Using mutation to create new user
+    const [createPost, {error}] = useMutation(CREATE_POST)
+    //   Saving values from form into formState
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await createPost({
+                variables: { postInput: formState },
+            });
+            console.log(data)
+          
+        }catch (e) {
+            console.error(e);
+        }
+        setFormState({
+            title: "",
+            image: "",
+            description: "",
+            price: ""
+        })
+    };
+
+  return (
+    <div className="container mt-5 signup-page">
+    <div className="row justify-content-center">
+      <div className="col-12 col-md-8 col-lg-6">
+        <div className="card signup-card">
+          <div className="card-body">
+            <h2 className="text-center mb-4">Add New Post</h2>
+            <form onSubmit={handleFormSubmit}>
+              <div className="form-group">
+                <label>Title</label>
+                <input
+                  type="text"
+                  name="title"
+                  value={formState.title}
+                  onChange={handleChange}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Image</label>
+                <input
+                  type="text"
+                  name="image"
+                  value={formState.image}
+                  onChange={handleChange}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Description</label>
+                <input
+                  type="text"
+                  name="description"
+                  value={formState.description}
+                  onChange={handleChange}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Price</label>
+                <input
+                  type="number"
+                  name="price"
+                  step="0.01"
+                  value={formState.price}
+                  onChange={handleChange}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-custom mt-4 w-100">
+                Publish
+              </button>
+            </form>
+            {error && <div className="text-danger mt-3">{error.message}</div>}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default CreatePost
